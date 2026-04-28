2d17530ea93ad8f367870e7fc13e0f5b7ba13511
Jenny Brun <s251873@dtu.dk>
2026-04-28 14:44:33 +0200

    EKFTracker with joint and sequential update

--- Project/EKF/EKFTracker.py
+++ Project/EKF/EKFTracker.py
@@ -1,4 +1,5 @@
 import numpy as np
+from scipy.linalg import block_diag
 
 class EKFTracker:
     def __init__(self, x0, P0, motion_model, frame_manager, initial_time=0.0):
@@ -15,13 +16,30 @@ class EKFTracker:
         self.x = F @ self.x
         self.P = F @ self.P @ F.T + Q
 
-    def update(self, sensor_id, z, time=None):
-        z_pred = self.frame_manager.compute_measurement(sensor_id, self.x)
-        H = self.frame_manager.compute_jacobian(sensor_id, self.x)
-        R = self.frame_manager.get_noise_covariance(sensor_id)
+    def update_joint(self, sensor_id_list, measurements, time=None):
+        z_list = []
+        z_pred_list = []
+        H_list = []
+        R_list = []
+
+        for sensor_id, z in zip(sensor_id_list, measurements):
+
+            z_pred = self.frame_manager.compute_measurement(sensor_id, self.x)
+            H = self.frame_manager.compute_jacobian(sensor_id, self.x)
+            R = self.frame_manager.get_noise_covariance(sensor_id)
+            z_list.append(z)
+            z_pred_list.append(z_pred)
+            H_list.append(H)
+            R_list.append(R)
+
+        z = np.concatenate(z_list)
+        z_pred = np.concatenate(z_pred_list)
+        H = np.vstack(H_list)
+        R = block_diag(*R_list)
 
         y = z - z_pred
-        y[1] = np.arctan2(np.sin(y[1]), np.cos(y[1]))  
+        for i in range(1, len(y), 2):
+            y[i] = np.arctan2(np.sin(y[i]), np.cos(y[i]))  
 
         S = H @ self.P @ H.T + R
         K = self.P @ H.T @ np.linalg.inv(S)
@@ -30,6 +48,30 @@ class EKFTracker:
         self.P = (np.eye(4) - K @ H) @ self.P
         self.state_history.append(self.x.copy())
         self.time_history.append(time)
+
+
+    def update(self, sensor_id, z, time=None):
+        z_pred = self.frame_manager.compute_measurement(sensor_id, self.x)
+        H = self.frame_manager.compute_jacobian(sensor_id, self.x)
+        R = self.frame_manager.get_noise_covariance(sensor_id)
+
+        if z_pred is None or H is None or R is None:
+            return None
+
+        y = z - z_pred
+        y[1] = np.arctan2(np.sin(y[1]), np.cos(y[1]))  
+
+        S = H @ self.P @ H.T + R
+        K = self.P @ H.T @ np.linalg.inv(S)
+
+        nis = float(y.T @ np.linalg.inv(S) @ y)
+
+        self.x = self.x + K @ y
+        self.P = (np.eye(4) - K @ H) @ self.P
+        self.state_history.append(self.x.copy())
+        self.time_history.append(time)
+        return nis
+
 
     def get_rmse(self, ground_truth):
         estimates = np.asarray(self.state_history, dtype=float)
